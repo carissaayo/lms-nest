d8ab0258c4b292f09181195f0e446635b4ab1d96
Ajao Yussuf <ajaoyussuayo@gmail.com>
2025-08-16 22:42:04 +0100

    still on course

--- src/app/course/course.controller.ts
+++ src/app/course/course.controller.ts
@@ -12,6 +12,7 @@ import {
   Req,
   Patch,
   ParseUUIDPipe,
+  Delete,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CourseService } from './course.service';
@@ -56,95 +57,14 @@ export class CourseController {
       req,
     );
   }
-  //   // Public routes
-  //   @Public()
-  //   @Get('filter')
-  //   filterCourses(@Query() query: any) {
-  //     return this.courseService.filterCourses(query);
-  //   }
-  //   @Public()
-  //   @Get(':id')
-  //   getSingleCourse(@Param('id') id: string) {
-  //     return this.courseService.getSingleCourse(id);
-  //   }
 
-  //   @Get('instructor/:instructor')
-  //   getAllCoursesByAnInstructor(@Param('instructor') instructor: string) {
-  //     return this.courseService.getAllCoursesByAnInstructor(instructor);
-  //   }
+  @Get()
+  async getCourses(@Query() query: any) {
+    return this.courseService.viewCourses(query);
+  }
 
-  //   @Get('course/:id')
-  //   getAllLecturesInACourse(@Param('id') id: string) {
-  //     return this.lectureService.getAllLecturesInACourse(id);
-  //   }
-
-  //   @Roles(Role.INSTRUCTOR)
-  //   @Post('create')
-  //   @UseInterceptors(FileInterceptor('image', { storage: diskStorage({}) }))
-  //   createCourse(
-  //     @Request() req: AuthenticatedRequest,
-  //     @Body() body: CreateCourseDto,
-  //     @UploadedFile() file: Express.Multer.File,
-  //   ) {
-  //     return this.courseService.createCourse(req, body, file);
-  //   }
-
-  //   @Roles(Role.INSTRUCTOR)
-  //   @Put(':id/update-course')
-  //   updateCourse(
-  //     @Param('id') id: string,
-  //     @Body() body: UpdateCourseDto,
-  //     @Request() req: AuthenticatedRequest,
-  //   ) {
-  //     return this.courseService.updateCourse(req, body, id);
-  //   }
-
-  //   @Roles(Role.INSTRUCTOR)
-  //   @Post(':id/submission')
-  //   submitCourseForApproval(
-  //     @Param('id') id: string,
-  //     @Request() req: AuthenticatedRequest,
-  //   ) {
-  //     return this.courseService.submitCourseForApproval(req, id);
-  //   }
-
-  //   @Roles(Role.INSTRUCTOR)
-  //   @Put(':id/delete-course')
-  //   deleteCourse(@Param('id') id: string, @Request() req: AuthenticatedRequest) {
-  //     return this.courseService.deleteCourse(req, id);
-  //   }
-
-  //   @Roles(Role.INSTRUCTOR)
-  //   @Put('publish-course/:id')
-  //   publishCourseByInstructor(
-  //     @Param('id') id: string,
-  //     @Request() req: AuthenticatedRequest,
-  //   ) {
-  //     return this.courseService.publishCourse(req, id);
-  //   }
-
-  //   @Roles(Role.INSTRUCTOR)
-  //   @Put(':id/lectures')
-  //   deleteAllLectureInACourse(
-  //     @Param('id') id: string,
-  //     @Request() req: AuthenticatedRequest,
-  //   ) {
-  //     return this.lectureService.deleteAllLectureInACourse(req, id);
-  //   }
-
-  //   // Admin/Moderator
-  //   @Roles(Role.MODERATOR)
-  //   @Put('approve-course/:id')
-  //   approveCourseByModerator(
-  //     @Param('id') id: string,
-  //     @Request() req: AuthenticatedRequest,
-  //   ) {
-  //     return this.courseService.approveCourse(req, id);
-  //   }
-
-  //   @Roles(Role.ADMIN)
-  //   @Get()
-  //   getAllCoursesAvailable(@Request() req: AuthenticatedRequest) {
-  //     return this.courseService.getAllCourses();
-  //   }
+  @Delete(':id')
+  async deleteCourse(@Param('id') courseId: string, @Req() req: CustomRequest) {
+    return this.courseService.deleteCourse(courseId, req);
+  }
 }
