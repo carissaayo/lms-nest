520a7a82bb1eba0fa2cd0bbc9cc9d9a09bacdcc4
Ajao Yussuf <ajaoyussuayo@gmail.com>
2025-08-19 17:12:47 +0100

    tomoa

--- src/app/admin/controllers/admin-courses.controller.ts
+++ src/app/admin/controllers/admin-courses.controller.ts
@@ -6,6 +6,8 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
 
 import { RolesGuard } from '../../common/guards/role.guard';
@@ -19,6 +21,8 @@ import { Permissions } from '../../common/decorators/permissions.decorator';
 import { AdminUserService } from '../services/admin-users.service';
 import { SuspendUserDTO } from '../admin.dto';
 import { PermissionsEnum } from '../admin.interface';
+import { AdminCoursesService } from '../services/admin-course.service';
+import { ApproveCourseDTO } from 'src/app/course/course.dto';
 
 @Controller('admin-courses')
 @UsePipes(
@@ -32,14 +36,19 @@ import { PermissionsEnum } from '../admin.interface';
 @Roles(UserRole.ADMIN)
 @Permissions(PermissionsEnum.ADMIN_COURSES)
 export class AdminCoursesController {
-  constructor(private adminUserService: AdminUserService) {}
+  constructor(private adminCoursesService: AdminCoursesService) {}
 
   @Patch(':courseId/action')
-  approveCourse(
+  async approveCourse(
     @Param('courseId') courseId: string,
-    @Body() suspendDto: SuspendUserDTO,
+    @Body() dto: ApproveCourseDTO,
     req: CustomRequest,
   ) {
-    return this.adminUserService.suspendUser(courseId, suspendDto, req);
+    return this.adminCoursesService.approveCourse(courseId, dto, req);
+  }
+
+  @Get()
+  async getCourses(@Query() query: any) {
+    return this.adminCoursesService.viewCourses(query);
   }
 }
