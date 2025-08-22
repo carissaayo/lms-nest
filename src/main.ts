1f70f6226b6d2717d4326052a2e1465afc551534
Ajao Yussuf <ajaoyussuayo@gmail.com>
2025-08-22 17:02:23 +0100

    done for today

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSecurity } from './app/security/setup-security.middleware';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   setupSecurity(app.getHttpAdapter().getInstance());
   await app.listen(process.env.PORT ?? 3000);
 }
