b15969edd86cfcd102daf69b796ee2f04aaaf00d
Yussuf <you@example.com>
2025-09-13 06:45:36 +0100

    plo

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { setupSecurity } from './app/security/setup-security.middleware';
 import { ValidationPipe } from '@nestjs/common';
 import bodyParser from 'body-parser';
 import { AllExceptionsFilter } from './libs/all-exception.filter';
-import config from './app/config/config';
-const appConfig = config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -30,7 +29,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['http://localhost:5173', appConfig.frontend_url],
+    origin: ['http://localhost:5173', 'https://lms-frontend-rsbi.vercel.app'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'RefreshToken'],
